45b17474cdb32555222c477307ebc28bf1f18147
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-07-14 17:28:18 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -85,7 +85,7 @@ export default function Home() {
         }
         const blob = await response.blob();
         const localUrl = URL.createObjectURL(blob as Blob);
-        setFormattedImage(localUrl);
+        setFormattedImage(input.current?.value);
       } catch (e) {
         if (e instanceof Error) {
           toast({
